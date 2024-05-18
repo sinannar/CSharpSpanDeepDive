3be9d4ef1e7a9d1e94e1dbd8f8702a788e4d9702
Sinan Nar <sinan.nar@gmail.com>
2024-05-19 01:41:10 +1200

    project, gitignore etc created

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,24 +1,12 @@
-﻿var array = Enumerable.Range(0, 1000).ToArray();
-var list = Enumerable.Range(0, 1000).ToList();
+﻿var list = Enumerable.Range(0, 1000).ToList();
 
 while (true)
 {
-    SumA(array, 0 , 50);
-    SumL(list, 0 , 50);
+    Sum(list, 0 , 50);
 }
 
 
-static int SumA(int[] array, int offset, int length)
-{
-    int sum = 0;
-    for (int i = offset; i < offset + length; i++)
-    {
-        sum += array[i];
-    }
-    return sum;
-}
-
-static int SumL(List<int> array, int offset, int length)
+static int Sum(IList<int> array, int offset, int length)
 {
     int sum = 0;
     for (int i = offset; i < offset + length; i++)
