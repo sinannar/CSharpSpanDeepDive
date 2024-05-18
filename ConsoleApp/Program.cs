03caf10ef1e0a0735579b1a606ef5a99930136e5
Sinan Nar <sinan.nar@gmail.com>
2024-05-19 01:32:04 +1200

    sum of first 50 with very bad code

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,16 +2,13 @@
 
 while (true)
 {
-    // Copy first 50 of numbers to new array
     int[] copy = new int[50];
     Array.Copy(array, 0, copy, 0, 50);
-
-
-    Sum(array);
+    Sum(copy);
 }
 
 
-static int Sum(int[] array, int offset, int length)
+static int Sum(int[] array)
 {
     int sum = 0;
     foreach (var i in array) sum += i;
