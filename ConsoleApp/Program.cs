8fcfeee90a47da6bddeb5dd437210870acc21838
Sinan Nar <sinan.nar@gmail.com>
2024-05-19 01:31:39 +1200

    sum of array

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = Enumerable.Range(0, 1000).ToArray();
+
+while (true)
+{
+    // Copy first 50 of numbers to new array
+    int[] copy = new int[50];
+    Array.Copy(array, 0, copy, 0, 50);
+
+
+    Sum(array);
+}
+
+
+static int Sum(int[] array, int offset, int length)
+{
+    int sum = 0;
+    foreach (var i in array) sum += i;
+    return sum;
+}
