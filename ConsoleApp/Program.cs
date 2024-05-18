264a44305645bddf3b97b63d488497a15ae5c036
Sinan Nar <sinan.nar@gmail.com>
2024-05-19 01:39:07 +1200

    array and list

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,24 @@
-﻿int[] array = Enumerable.Range(0, 1000).ToArray();
+﻿var array = Enumerable.Range(0, 1000).ToArray();
+var list = Enumerable.Range(0, 1000).ToList();
 
 while (true)
 {
-    Sum(array, 0 , 50);
+    SumA(array, 0 , 50);
+    SumL(list, 0 , 50);
 }
 
 
-static int Sum(int[] array, int offset, int length)
+static int SumA(int[] array, int offset, int length)
+{
+    int sum = 0;
+    for (int i = offset; i < offset + length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+static int SumL(List<int> array, int offset, int length)
 {
     int sum = 0;
     for (int i = offset; i < offset + length; i++)
