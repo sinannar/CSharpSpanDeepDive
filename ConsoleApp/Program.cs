826569453bba7536ce1d436f2209380d9b5077ab
Sinan Nar <sinan.nar@gmail.com>
2024-05-19 02:05:18 +1200

    broken but it validate that you can copy array into another variable

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,19 @@ int[] three = [42, 43, 44];
 // C# compiler generate UTF8 bytes from this string and blitting those directly to assembly and span just pointing to that data in binary
 ReadOnlySpan<byte> span = "Hello, Sinan"u8; 
 
+
+static string Create(Action<char[]> action)
+{
+
+}
+
+Create(array => { 
+    array[0] = 'H';
+    s_array = array;
+});
+char[] s_array;
+
+
 Sum(array);
 Sum(new Span<int>(array, 0, 50));
 
