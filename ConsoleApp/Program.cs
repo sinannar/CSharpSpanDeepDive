3ce31c55a502293ce60546a6e39f99e876fcbfd1
Sinan Nar <sinan.nar@gmail.com>
2024-05-19 02:10:34 +1200

    use of ref

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,41 +1,54 @@
-﻿int[] array = new int[1000];
+﻿//int[] array = new int[1000];
 
-int[] three = [42, 43, 44];
+//int[] three = [42, 43, 44];
 
-// C# compiler generate UTF8 bytes from this string and blitting those directly to assembly and span just pointing to that data in binary
-ReadOnlySpan<byte> span = "Hello, Sinan"u8;
+//// C# compiler generate UTF8 bytes from this string and blitting those directly to assembly and span just pointing to that data in binary
+//ReadOnlySpan<byte> span = "Hello, Sinan"u8;
 
 
-string.Create(34, Guid.NewGuid(), (span, guid) =>
-{
-    "ID".AsSpan().CopyTo(span);
-    guid.TryFormat(span.Slice(2), out _, "N");
-});
-
-//static string Create(Action<char[]> action)
+//string.Create(34, Guid.NewGuid(), (span, guid) =>
 //{
+//    "ID".AsSpan().CopyTo(span);
+//    guid.TryFormat(span.Slice(2), out _, "N");
+//});
 
+////static string Create(Action<char[]> action)
+////{
+
+////}
+
+////Create(array => { 
+////    array[0] = 'H';
+////    s_array = array;
+////});
+////char[] s_array;
+
+
+//Sum(array);
+//Sum(new Span<int>(array, 0, 50));
+
+//unsafe
+//{
+//    int* ptr = stackalloc int[1000];
+//    Sum(new Span<int>(ptr, 1000));
 //}
 
-//Create(array => { 
-//    array[0] = 'H';
-//    s_array = array;
-//});
-//char[] s_array;
+//static unsafe int Sum(Span<int> span)
+//{
+//    int sum = 0;
+//    foreach (int value in span) sum += value;
+//    return sum;
+//}
 
 
-Sum(array);
-Sum(new Span<int>(array, 0, 50));
 
-unsafe
+int i = 0;
+
+Console.WriteLine(i);
+Use(ref i);
+Console.WriteLine(i);
+
+static void Use(ref int i)
 {
-    int* ptr = stackalloc int[1000];
-    Sum(new Span<int>(ptr, 1000));
-}
-
-static unsafe int Sum(Span<int> span)
-{
-    int sum = 0;
-    foreach (int value in span) sum += value;
-    return sum;
+    i = 42;
 }
